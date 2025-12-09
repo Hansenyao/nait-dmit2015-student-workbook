564ae49f67a76a3016f222c2d24f15505698d1f5
Youfang Yao <yyfsingler@163.com>
2025-12-08 19:04:13 -0700

    finished

--- dmit2015-etl-process-dwpubssales/src/main/java/dmit2015/config/ApplicationConfig.java
+++ dmit2015-etl-process-dwpubssales/src/main/java/dmit2015/config/ApplicationConfig.java
@@ -1,15 +1,15 @@
-package common.config;
+package dmit2015.config;
 
 import jakarta.annotation.sql.DataSourceDefinition;
 import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class ApplicationConfig {
-}
 
 @DataSourceDefinition(
         name="java:app/datasources/MSSQLServerDWPubsSalesDS",
         className="com.microsoft.sqlserver.jdbc.SQLServerDataSource",
         url="jdbc:sqlserver://localhost;databaseName=DWPubsSales;TrustServerCertificate=true",
         user="user2015",
-        password="Password2015"),
+        password="Password2015")
+
+@ApplicationScoped
+public class ApplicationConfig {
+}
